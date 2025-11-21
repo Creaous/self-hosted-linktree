2ef318ef2738645fda689a15b90cd830bef2571c
Creaous <68230846+Creaous@users.noreply.github.com>
2025-11-21 23:58:09 +1030

    make default values generic

--- src/values.ts
+++ src/values.ts
@@ -2,39 +2,39 @@ import { type Config } from '$lib';
 import { CoffeeIcon, GithubIcon, HeartIcon, TwitterIcon } from '@lucide/svelte';
 
 export const config: Config = {
-  username: 'Mitchell',
-  description: 'Software Engineer',
+  username: 'Your name goes here!',
+  description: 'A little description about yourself.',
   links: [
     {
       name: 'GitHub',
-      url: 'https://github.com/Creaous',
+      url: 'https://github.com/example.invalid',
       icon: GithubIcon,
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/XeniNexi',
+      url: 'https://twitter.com/example.invalid',
       icon: TwitterIcon,
     },
     {
       name: 'Bluesky',
-      url: 'https://bsky.app/profile/creaous.net',
+      url: 'https://bsky.app/profile/example.invalid',
       icon: GithubIcon,
     },
     {
       name: 'Github Sponsors',
-      url: 'https://github.com/sponsors/Creaous',
+      url: 'https://github.com/sponsors/example.invalid',
       icon: HeartIcon,
     },
     {
       name: 'Buy Me a Coffee',
-      url: 'https://buymeacoffee.com/creaous',
+      url: 'https://buymeacoffee.com/example.invalid',
       icon: CoffeeIcon,
     },
   ],
   extra: {
     bottom: {
       content: `
-      <p class="text-base-content/40">Information should be free and accessible.</p>
+      <p class="text-base-content/40">An additional message at the bottom if you'd like!</p>
       `,
       isHtml: true,
     },
